6dd9b6ba20f249529014b113c60ec60877d411f8
titanium <titaniume@163.com>
2017-12-07 00:56:30 +0800

    web应用开发-模板引擎FreeMarker

---
+++ springboot-demo-8/src/main/java/com/titaniume/springboot/SpringbootDemo8Application.java
@@ -0,0 +1,12 @@
+package com.titaniume.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo8Application.class, args);
+	}
+}
