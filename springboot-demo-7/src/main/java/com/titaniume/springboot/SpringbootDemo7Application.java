d760e33c9d847a3d7c46802b8da828d0910c9056
titanium <titaniume@163.com>
2017-12-06 23:51:09 +0800

    日志配置-log4j2

---
+++ springboot-demo-7/src/main/java/com/titaniume/springboot/SpringbootDemo7Application.java
@@ -0,0 +1,12 @@
+package com.titaniume.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo7Application.class, args);
+	}
+}
