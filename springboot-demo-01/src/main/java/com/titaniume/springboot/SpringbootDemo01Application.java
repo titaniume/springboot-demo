378030282ea795fbbb5c4fe5d92bd1325de10de6
titanium <titaniume@163.com>
2017-12-07 01:28:00 +0800

    RESTfull API简单项目的快速搭建

--- springboot-demo-01/src/main/java/com/titaniume/springboot/SpringbootDemo01Application.java
+++ springboot-demo-01/src/main/java/com/titaniume/springboot/SpringbootDemo01Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootDemo1Application {
+public class SpringbootDemo01Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemo1Application.class, args);
+		SpringApplication.run(SpringbootDemo01Application.class, args);
 	}
 	
 	
