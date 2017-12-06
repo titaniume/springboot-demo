f5d28d92c067192f7b44924b0eb6c7020af350c8
titanium <titaniume@163.com>
2017-12-07 01:43:35 +0800

    web应用开发-模板引擎Thymeleaf

--- springboot-demo-09/src/main/java/com/titaniume/springboot/SpringbootDemo09Application.java
+++ springboot-demo-09/src/main/java/com/titaniume/springboot/SpringbootDemo09Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootDemo9Application {
+public class SpringbootDemo09Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemo9Application.class, args);
+		SpringApplication.run(SpringbootDemo09Application.class, args);
 	}
 }
