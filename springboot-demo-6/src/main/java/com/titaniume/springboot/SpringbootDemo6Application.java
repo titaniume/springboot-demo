118d3646c002480e435ca39abce0ac01eb0ca536
titanium <titaniume@163.com>
2017-12-06 23:33:59 +0800

    日志配置-logback

---
+++ springboot-demo-6/src/main/java/com/titaniume/springboot/SpringbootDemo6Application.java
@@ -0,0 +1,13 @@
+package com.titaniume.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemo6Application.class, args);
+	}
+}
+ 
