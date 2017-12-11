1a1a2ea404cf397a3a633f5df7f23f1d025db25a
titanium <titaniume@163.com>
2017-12-12 00:20:12 +0800

    使用 SQL关系型数据库-h2 嵌入式数据库的使用

--- springboot-demo-18/src/test/java/com/titaniume/springboot/SpringbootDemo18ApplicationTests.java
+++ springboot-demo-18/src/test/java/com/titaniume/springboot/SpringbootDemo18ApplicationTests.java
@@ -1,22 +1,35 @@
 package com.titaniume.springboot;
 
-import java.util.Date;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.DefaultProxyRoutePlanner;
+import org.apache.http.protocol.HttpContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import com.titaniume.springboot.bean.UserLog;
-import com.titaniume.springboot.dao.UserLogDao;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootDemo18ApplicationTests {
 
 
-	@Autowired
+	/*@Autowired
 	private UserLogDao userLogDao;
 	
 	//JpaRepository 接口自带的查询
@@ -49,4 +62,66 @@ public class SpringbootDemo18ApplicationTests {
 		UserLog result = userLogDao.findOne(1);
 		System.out.println(result);
 	}
+	*/
+	
+	
+	//////////////////测试REST服务-使用代理///////////////////////////
+	
+	@Autowired
+	private RestTemplateBuilder restTemplateBuilder;
+
+	/**
+	 * get请求
+	 */
+	@Test
+	public void getForObject() {
+		UserLog bean = restTemplateBuilder.build().getForObject("http://localhost:8080/rest/update/{id}", UserLog.class, 1);
+		System.out.println(bean);
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("id", 7);
+		bean = restTemplateBuilder.build().postForObject("http://localhost:8080/rest/update", map, UserLog.class);
+		System.out.println(bean);
+	}
+	
+	
+	/**
+	 * 代理测试
+	 */
+	@Test
+	public void getProxy() {
+		String result = restTemplateBuilder.additionalCustomizers(new ProxyCustomizer()).build().getForObject("http://www.jd.com", String.class);
+		System.out.println(result);
+	}
+	
+	/**
+	 * 代理实现
+	 * @author titaniume
+	 * 2017年12月11日下午11:30:29
+	 *
+	 */
+	static class ProxyCustomizer implements RestTemplateCustomizer {
+		@Override
+		public void customize(RestTemplate restTemplate) {
+			String proxyHost = "210.13.77.66";
+			int proxyPort = 8080;
+			
+			HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+			HttpClient httpClient = HttpClientBuilder.create().setRoutePlanner(new DefaultProxyRoutePlanner(proxy) {
+				@Override
+				public HttpHost determineProxy(HttpHost target, HttpRequest request, HttpContext context) throws HttpException {
+					System.out.println(target.getHostName());
+					return super.determineProxy(target, request, context);
+				}
+			}).build();
+			HttpComponentsClientHttpRequestFactory httpComponentsClientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+			//连接超时
+			httpComponentsClientHttpRequestFactory.setConnectTimeout(10000);
+			//读取超时 这两个参数必须要设置
+			httpComponentsClientHttpRequestFactory.setReadTimeout(60000);
+			restTemplate.setRequestFactory(httpComponentsClientHttpRequestFactory);
+		}
+	}
+	
+	
+	
 }
